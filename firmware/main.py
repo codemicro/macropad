95e533c48c3f768dd989a7b64e7e6368d1d8a565
Tom <tom@tdpain.net>
2021-01-31 18:54:38 +0000

    Switch to using GP13 tied high

--- firmware/main.py
+++ firmware/main.py
@@ -9,25 +9,21 @@ from adafruit_hid.keyboard import Keyboard
 from adafruit_hid.keycode import Keycode
 from adafruit_hid.keyboard_layout_us import KeyboardLayoutUS
 
-delay = 0.5
-
 led = digitalio.DigitalInOut(board.GP25)
 led.direction = digitalio.Direction.OUTPUT
 
-inp_pin = digitalio.DigitalInOut(board.GP15)
+inp_pin = digitalio.DigitalInOut(board.GP13)
 inp_pin.direction = digitalio.Direction.INPUT
-inp_pin.pull = digitalio.Pull.DOWN
+inp_pin.pull = digitalio.Pull.UP
 
 button = db.Debouncer(inp_pin)
+
 kbd = Keyboard(usb_hid.devices)
-
 uskb = KeyboardLayoutUS(kbd)
-
-led_state = False
 
 while True:
     button.update()
-    if button.rose:
+    if button.fell:
         kbd.send(Keycode.WINDOWS, Keycode.R)
         time.sleep(0.25)
         uskb.write("chrome.exe")
