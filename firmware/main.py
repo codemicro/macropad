7978b346150b27778f1d269d7433a9c7b2777786
Tom <tom@tdpain.net>
2021-01-31 14:30:40 +0000

    Switch to CircuitPython, add bundler.json for dependencies

--- firmware/main.py
+++ firmware/main.py
@@ -1,18 +1,35 @@
-# Simple script to use a pushbutton on pin 15 to toggle the integrated LED on pin 25
+import digitalio
+import board
 
-from machine import Pin
+import adafruit_debouncer as db
 import time
 
-last = time.ticks_ms()
+import usb_hid
+from adafruit_hid.keyboard import Keyboard
+from adafruit_hid.keycode import Keycode
+from adafruit_hid.keyboard_layout_us import KeyboardLayoutUS
 
-p2 = Pin(15, Pin.IN, Pin.PULL_DOWN)
-led = Pin(25, Pin.OUT)
+delay = 0.5
 
-def interrupt(p):
-    global last
-    tx = time.ticks_ms()
-    if tx-last > 225:
-        last = tx
-        led.toggle()
+led = digitalio.DigitalInOut(board.GP25)
+led.direction = digitalio.Direction.OUTPUT
 
-p2.irq(interrupt, trigger=Pin.IRQ_RISING)
+inp_pin = digitalio.DigitalInOut(board.GP15)
+inp_pin.direction = digitalio.Direction.INPUT
+inp_pin.pull = digitalio.Pull.DOWN
+
+button = db.Debouncer(inp_pin)
+kbd = Keyboard(usb_hid.devices)
+
+uskb = KeyboardLayoutUS(kbd)
+
+led_state = False
+
+while True:
+    button.update()
+    if button.rose:
+        kbd.send(Keycode.WINDOWS, Keycode.R)
+        time.sleep(0.25)
+        uskb.write("chrome.exe")
+        kbd.send(Keycode.ENTER)
+        led.value = not led.value
